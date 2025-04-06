4f000ed617dd50c82bfc6c39539533083f6d45eb
gorox66 <siotka@list.ru>
2025-04-06 12:41:17 +0300

    Изменена функция add_book, убран класс Book

--- project.py
+++ project.py
@@ -1,15 +1,15 @@
-class Book:
-    def __init__(self, title, author, year, genre, quantity):
-        self.title = title
-        self.author = author
-        self.year = year
-        self.genre = genre
-        self.quantity = quantity
+def add_book():
+    name_of_book = input("Введите название книги: ")
+    author = input("Ввеите автора книги: ")
+    year_of_publication = int(input("Введите год издания книги: "))
+    genre = input("Введите жанр книги: ")
+    count_books = int(input("Введите количество кинг в наличии: "))
 
-    def __str__(self):
-        return f"{self.title};{self.author};{self.year};{self.genre};{self.quantity}"
+    book = f"{name_of_book}; {author}; {year_of_publication}; {genre}; {count_books}"
 
-def add_book(book):
-    file = open('catalog.txt', 'a', encoding='utf-8')
-    file.write(str(book) + '\n')
+    file = open("catalog.txt", "a", encoding="utf-8")
+    file.write(book)
     file.close()
+
+    print("Книга добавлена в каталог")
+
