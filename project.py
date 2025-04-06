60801eee12f9cebe2e2c27eb5c0c4c3aba54f25a
gorox66 <siotka@list.ru>
2025-04-06 13:03:22 +0300

    Добавлена функция show_books, которая выводит каталог книг

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ def add_book():
     genre = input("Введите жанр книги: ")
     count_books = int(input("Введите количество кинг в наличии: "))
 
-    book = f"{name_of_book}; {author}; {year_of_publication}; {genre}; {count_books}"
+    book = f"{name_of_book};{author};{year_of_publication};{genre};{count_books}\n"
 
     file = open("catalog.txt", "a", encoding="utf-8")
     file.write(book)
@@ -13,3 +13,15 @@ def add_book():
 
     print("Книга добавлена в каталог")
 
+def show_books():
+    file = open("catalog.txt", "r", encoding="utf-8")
+    books = file.readlines()
+    file.close()
+
+    if books:
+        print("Список книг:")
+        for book in books:
+            name_of_book, author, year_of_publication, genre, count_books = book.strip().split(';')
+            print(f"Название книги: {name_of_book}, автор: {author}, год издания: {year_of_publication}, жанр: {genre}, количество книг в наличии: {count_books}")
+    else:
+        print("В каталоге нет книг")
