83d85846ac6cb4647759987435e040f32b57bd4b
gorox66 <siotka@list.ru>
2025-04-06 13:54:42 +0300

    Добавлена функция edit_book, которая меняет данные книги по ее названию

--- project.py
+++ project.py
@@ -51,3 +51,63 @@ def remove_book():
         print("Книга удалена из каталога")
     else:
         print("Книга не найдена в каталоге")
+
+def edit_book():
+    name_of_book = input("Введите название книги для редактирования: ")
+
+    file = open("catalog.txt", "r", encoding="utf-8")
+    books = file.readlines()
+    file.close()
+
+    updated_books = []
+    book_found = False
+
+    for book in books:
+        current_name_of_book = book.split(';')[0]
+        if current_name_of_book == name_of_book:
+            book_found = True
+            print("Текущая информация о книге:")
+            print(book.strip())
+
+            new_name = input("Введите новое название книги (оставьте пустым, если изменения не нужны)")
+            new_author = input("Введите нового автора (оставьте пустым, если изменения не нужны): ")
+            new_year = input("Введите новый год издания (оставьте пустым, если изменения не нужны): ")
+            new_genre = input("Введите новый жанр (оставьте пустым, если изменения не нужны): ")
+            new_count = input("Введите новое количество книг (оставьте пустым, если изменения не нужны): ")
+
+            if new_name:
+                current_name_of_book = new_name
+
+            if new_author:
+                author = new_author
+            else:
+                author = book.split(';')[1]
+
+            if new_year:
+                year_of_publication = new_year
+            else:
+                year_of_publication = book.split(';')[2]
+
+            if new_genre:
+                genre = new_genre
+            else:
+                genre = book.split(';')[3]
+
+            if new_count:
+                count_books = new_count
+            else:
+                count_books = book.split(';')[4].strip()
+
+            updated_book = f"{current_name_of_book};{author};{year_of_publication};{genre};{count_books}\n"
+            updated_books.append(updated_book)
+        else:
+            updated_books.append(book)
+
+    file = open("catalog.txt", "w", encoding="utf-8")
+    file.writelines(updated_books)
+    file.close()
+
+    if book_found:
+        print("Информация о книге обновлена")
+    else:
+        print("Книга не найдена в каталоге")
